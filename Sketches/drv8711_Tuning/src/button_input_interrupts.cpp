1178f971731d0b29baa26631331e15eb816a57ff
James Jenkins <james.jenkins745@protonmail.com>
2024-03-12 15:37:30 -0500

    working code for HybridPolling

--- Sketches/drv8711_Tuning/src/button_input_interrupts.cpp
+++ Sketches/drv8711_Tuning/src/button_input_interrupts.cpp
@@ -19,89 +19,20 @@ const byte M1_BOTTOM_LIMIT_SWITCH = 11;
 // const byte M2_BOTTOM_LIMIT_SWITCH = ;
 
 // Initialize volatile variables
-volatile byte state_of_EMERGENCY_BUTTON = 0;
-volatile byte state_of_ENABLE_BUTTON = 0;
-volatile byte state_of_M1_CW_BUTTON = 0;
-volatile byte state_of_M1_CCW_BUTTON = 0;
 volatile byte state_of_M1_TOP_LIMIT_SWITCH = 0;
 volatile byte state_of_M1_BOTTOM_LIMIT_SWITCH = 0;
 volatile byte lastButtonState = HIGH;
 volatile byte button_state;
+volatile byte reading;
 // Use when ready to test M2
-// volatile byte state_of_M2_CW_BUTTON = 0;
-// volatile byte state_of_M2_CCW_BUTTON = 0;
 // volatile byte state_of_M2_TOP_LIMIT_SWITCH = 0;
 // volatile byte state_of_M2_BOTTOM_LIMIT_SWITCH = 0;
 
 
 // Initialize Variables
 const uint8_t rotations_per_button_press = 2; // Set how many rotations you want a button press to go here.
-//unsigned long steps_per_button_press = rotations_per_button_press * number_of_steps_for_one_rotation;// The max value this could be is 256 step_mode * 200 full steps per rotation * 60 rotations from one end of the track to the other end = 3,072,000 1/256 steps.
-unsigned long steps_per_button_press = 40000;
-uint32_t lastsent;
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 unsigned long debounceDelay = 0;    // the debounce time; increase if the output flickers
-
-// To-do: Decide and select appropriate outcome for emergency stop and enable.
-// Define the actions for each button
-void actionForButton(byte buttonPin)
-{
-  unsigned int counter = 0; // For CW and CCW rotation.
-
-  switch (buttonPin)
-  {
-  case EMERGENCY_BUTTON:
-    // Disable stepper motor
-    Serial.println("Inside emergency button routine.");
-    disableMotor();
-    state_of_EMERGENCY_BUTTON = 0;
-    break;
-
-  case ENABLE_BUTTON:
-    // Enable stepper motor
-    Serial.println("Inside ENABLE button routine.");
-    changeMotorState();
-    state_of_ENABLE_BUTTON = 0;
-    break;
-
-  case M1_CW_BUTTON:
-    // Rotate the stepper motor in CW direction
-    digitalWrite(M1_DirectionPin, HIGH);
-    while ((counter < steps_per_button_press) && (state_of_M1_TOP_LIMIT_SWITCH == 0))
-    {
-      takeStep();
-      counter++;
-    }
-    state_of_M1_CW_BUTTON = 0;
-    // Clear the limit switch flag after a successful rotation. The carriage should no longer be on the limit switch.
-    if (state_of_M1_BOTTOM_LIMIT_SWITCH != 0)
-    {
-      state_of_M1_BOTTOM_LIMIT_SWITCH = 0;
-    }
-    break;
-
-  case M1_CCW_BUTTON:
-    // Rotate the stepper motor in CCW direction
-    digitalWrite(M1_DirectionPin, LOW);
-    while ((counter < steps_per_button_press) && (state_of_M1_BOTTOM_LIMIT_SWITCH == 0))
-    {
-      takeStep();
-      counter++;
-    }
-    state_of_M1_CCW_BUTTON = 0;
-    // Clear the limit switch flag after a successful rotation. The carriage should no longer be on the limit switch.
-    if (state_of_M1_TOP_LIMIT_SWITCH != 0)
-    {
-      state_of_M1_TOP_LIMIT_SWITCH = 0;
-    }
-    break;
-    
-  default: // To-do: create a default case that is more relavent for when the system is operating in production environment.
-    // If we enter here message Error
-    Serial.println("There has been an error within the switch statement.");
-    break;
-  }
-}
 
 
 
@@ -110,26 +41,6 @@ void emergencyStop()
 {
   // EMERGENCY_BUTTON has been pressed.
   disableMotor();
-}
-
-void requestCWRotation()
-{
-  // Check if the corresponding limit switch is NOT pressed down.
-  // Set flag to enable rotation. Otherwise, ignore request.
-  if (state_of_M1_TOP_LIMIT_SWITCH == 0)
-  {
-    state_of_M1_CW_BUTTON = M1_CW_BUTTON;
-  }
-}
-
-void requestCCWRotation()
-{
-  // Check if the corresponding limit switch is NOT pressed down.
-  // Set flag to enable rotation. Otherwise, ignore request.
-  if (state_of_M1_BOTTOM_LIMIT_SWITCH == 0)
-  {
-    state_of_M1_CCW_BUTTON = M1_CCW_BUTTON;
-  }
 }
 
 void topLimitSwitchTriggered()
@@ -169,9 +80,6 @@ void setupButtons()
   // Arduino Interrupts allow for LOW, HIGH, CHANGE, RISING, AND FALLING as valid modes.
   // The PinChangeInterrupt library has RISING, FALLING, and CHANGE as valid modes of operation.
   attachInterrupt(digitalPinToInterrupt(EMERGENCY_BUTTON), emergencyStop, FALLING);
-  //attachInterrupt(digitalPinToInterrupt(ENABLE_BUTTON), enableMotors, FALLING);
-  //attachPCINT(digitalPinToPCINT(M1_CW_BUTTON), requestCWRotation, FALLING);
-  //attachPCINT(digitalPinToPCINT(M1_CCW_BUTTON), requestCCWRotation, FALLING);
   attachPCINT(digitalPinToPCINT(M1_TOP_LIMIT_SWITCH), topLimitSwitchTriggered, FALLING);
   attachPCINT(digitalPinToPCINT(M1_BOTTOM_LIMIT_SWITCH), bottomLimitSwitchTriggered, FALLING);
   // To-do: Add the remaing input buttons ISRs.
@@ -180,7 +88,7 @@ void setupButtons()
 
 void checkButtonState()
 {
-  volatile byte reading = digitalRead(ENABLE_BUTTON);
+  reading = digitalRead(ENABLE_BUTTON);
 
   if (reading != lastButtonState){lastDebounceTime = millis();}
   if ((millis() - lastDebounceTime) > debounceDelay){
@@ -188,45 +96,31 @@ void checkButtonState()
       button_state = reading;
       if (button_state == LOW){
         changeMotorState();
-        Serial.println("rotations_per_button_press: " + String(rotations_per_button_press));
-  Serial.println("steps_per_button_press: " + String(steps_per_button_press));
-  Serial.println("rotations_per_button_press: " + String(rotations_per_button_press));
-  Serial.println("number_of_steps_for_one_rotation: " + String(number_of_steps_for_one_rotation));
       }
     }
   }
 
-  //unsigned int counter1 = 0; // For CW and CCW rotation.
-  unsigned int counter2 = 0; // For CW and CCW rotation.
 
   if(!digitalRead(M1_CW_BUTTON))
   {
     digitalWrite(M1_DirectionPin, HIGH);
-    Timer1.pwm(12, 512);
-    uint32_t now = millis();
-    while (((now + 1000U) > millis()) && (digitalRead(M1_TOP_LIMIT_SWITCH)))
-    {
-      //Serial.println("counter1: " + String(counter1));
-    }
-    lastsent = now;
     Timer1.pwm(12,0);
-    // if(state_of_M1_TOP_LIMIT_SWITCH != 0)
-    // {
-    //   state_of_M1_TOP_LIMIT_SWITCH = 0;
-    // }
+    uint32_t now1 = millis();
+    while (((now1 + 1000U) > millis()) && (digitalRead(M1_TOP_LIMIT_SWITCH)))
+    {
+      Timer1.pwm(12, 512);
+    }
+    Timer1.pwm(12,0);
   }
   if(!digitalRead(M1_CCW_BUTTON))
   {
     digitalWrite(M1_DirectionPin, LOW);
-    while ((counter2 < steps_per_button_press) && (state_of_M1_BOTTOM_LIMIT_SWITCH == 0))
+    Timer1.pwm(12,0);
+    uint32_t now2 = millis();
+    while (((now2 + 1000U) > millis()) && (digitalRead(M1_BOTTOM_LIMIT_SWITCH)))
     {
       Timer1.pwm(12, 512);
-      counter2++;
     }
     Timer1.pwm(12, 0);
-    // if(state_of_M1_BOTTOM_LIMIT_SWITCH != 0)
-    // {
-    //   state_of_M1_BOTTOM_LIMIT_SWITCH = 0;
-    // }
   }
 }
